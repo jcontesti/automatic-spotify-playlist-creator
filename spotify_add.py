e5686ed2d2ac0bdd317407422ae4a6c662c0afe0
jcontesti <25779507+jcontesti@users.noreply.github.com>
2018-12-29 19:46:13 +0100

    Remove tracks of the playlist only when they have been moved out of the chart

--- spotify_add.py
+++ spotify_add.py
@@ -5,10 +5,13 @@ import spotipy.util as util
 
 import settings
 
+# Manual transformations of misspellings
 ARTISTS_NAMES_TRANSFORMATIONS = {
     '480 east': 'four80east',
     'india arie': 'india.arie',
 }
+
+SCOPE = 'playlist-modify-public'
 
 
 def get_chart():
@@ -47,46 +50,70 @@ def process_song(artist, track_name):
     return artists_names_transformed, tracks_names
 
 
-scope = 'playlist-modify-public'
-token = util.prompt_for_user_token(
-    settings.SPOTIFY_USERNAME,
-    scope=scope,
-    client_id=settings.SPOTIPY_CLIENT_ID,
-    client_secret=settings.SPOTIPY_CLIENT_SECRET,
-    redirect_uri=settings.SPOTIPY_REDIRECT_URI)
+def get_playlist_current_tracks():
+    current_tracks = []
 
-sp = spotipy.Spotify(auth=token)
-
-# empty playlist
-current_songs = sp.user_playlist(settings.SPOTIFY_USERNAME,
-            playlist_id=settings.SPOTIFY_PLAYLIST)
-
-for song in current_songs['tracks']['items']:
-
-    track_id = song['track']['id']
-
-    sp.user_playlist_remove_all_occurrences_of_tracks(
+    current_playlist_tracks = sp.user_playlist_tracks(
         settings.SPOTIFY_USERNAME,
-        playlist_id=settings.SPOTIFY_PLAYLIST,
-        tracks=[track_id],
-    )
+        playlist_id=settings.SPOTIFY_PLAYLIST)
 
-# fill playlist
-for song in get_chart():
-    artists_names, tracks_names = process_song(song['artist'], song['title'])
+    for current_track in current_playlist_tracks['items']:
+        current_tracks.append(current_track['track']['id'])
 
-    for artist_name in artists_names:
-        for track_name in tracks_names:
-            q = 'artist:"' + artist_name + '" track:"' + track_name + '"'
+    return current_tracks
 
-            print(q)
 
-            results = sp.search(q=q, type='track', limit=1)
+def update_playlist(sp):
 
-            if results['tracks']['items']:
-                track_id = results['tracks']['items'][0]['id']
+    tracks_to_load = []
 
-                sp.user_playlist_add_tracks(
-                    settings.SPOTIFY_USERNAME,
-                    playlist_id=settings.SPOTIFY_PLAYLIST,
-                    tracks=[track_id])
+    for song in get_chart():
+        artists_names, tracks_names = process_song(song['artist'],
+                                                   song['title'])
+
+        for artist_name in artists_names:
+            for track_name in tracks_names:
+                q = 'artist:"' + artist_name + '" track:"' + track_name + '"'
+
+                print(q)
+
+                results = sp.search(q=q, type='track', limit=1)
+
+                if results['tracks']['items']:
+                    tracks_to_load.append(results['tracks']['items'][0]['id'])
+
+    current_tracks = get_playlist_current_tracks()
+
+    # Remove all current tracks that have gone out of the chart
+    for current_track in current_tracks:
+        if current_track not in tracks_to_load:
+            # Remove the track
+            sp.user_playlist_remove_all_occurrences_of_tracks(
+                settings.SPOTIFY_USERNAME,
+                playlist_id=settings.SPOTIFY_PLAYLIST,
+                tracks=[current_track],
+            )
+
+    # Add all the new tracks that weren't on the playlist
+    for track_to_load in tracks_to_load:
+        if track_to_load not in current_tracks:
+            # Add the track
+            sp.user_playlist_add_tracks(
+                settings.SPOTIFY_USERNAME,
+                playlist_id=settings.SPOTIFY_PLAYLIST,
+                tracks=[track_to_load],
+            )
+
+
+if __name__ == "__main__":
+    token = util.prompt_for_user_token(
+        settings.SPOTIFY_USERNAME,
+        scope=SCOPE,
+        client_id=settings.SPOTIPY_CLIENT_ID,
+        client_secret=settings.SPOTIPY_CLIENT_SECRET,
+        redirect_uri=settings.SPOTIPY_REDIRECT_URI)
+
+    sp = spotipy.Spotify(auth=token)
+
+    update_playlist(sp)
+
