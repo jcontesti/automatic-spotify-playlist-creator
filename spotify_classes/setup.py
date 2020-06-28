980d4ebc964ab25a6208ff89e073712873183667
jcontesti <25779507+jcontesti@users.noreply.github.com>
2020-06-28 16:20:48 +0200

    Add setup.py

---
+++ spotify_classes/setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+setup(
+    name='scrapy-to-spotify',
+    version='',
+    packages=[''],
+    package_dir={'': 'spotify_classes'},
+    url='',
+    license='MIT',
+    author='Jordi Contestí',
+    author_email='',
+    description=''
+)
