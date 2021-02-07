0e5eef26f63190a5a68b49dddf9757e86c30e34d
jcontesti <25779507+jcontesti@users.noreply.github.com>
2021-02-07 23:45:22 +0100

    Add starpoint_uk_soul_chart_breakers scrapping, remove solar_radio_soul_energy_house_chart

---
+++ examples/starpoint_uk_soul_chart_breakers_spider.py
@@ -0,0 +1,62 @@
+"""Class to implement Starpoint Radio UK Soul Chart Scrapy spider."""
+from typing import Dict, Generator
+
+import re
+import scrapy
+from scrapy.http import TextResponse
+
+from extractors.scrapy_extractor import ScrapyExtractor
+
+
+class StarpointUKSoulChartBreakersSpider(
+    scrapy.Spider,  # type: ignore
+    ScrapyExtractor
+):
+    """Class to implement Starpoint Radio UK Soul Chart Scrapy spider."""
+    name = "StarpointUKSoulChartBreakersSpider"
+    start_urls = [
+        "http://uksoulchart.com/show_chart_breakers.php",
+    ]
+
+    ALBUM_INDICATORS = [" album", "/Album", "/album"]
+
+    def _remove_album_indicator(self, song_title: str) -> str:
+        song_title_wo_album_indicator: str = song_title
+        for album_indicator in self.ALBUM_INDICATORS:
+            song_title_wo_album_indicator = (
+                song_title_wo_album_indicator.replace(album_indicator, "")
+            )
+
+        return song_title_wo_album_indicator
+
+    @staticmethod
+    def _remove_text_between_parentheses(song_title: str) -> str:
+        return re.sub("[\(\[].*?[\)\]]", "", song_title)
+
+    def _extract_album(self, song_title: str) -> str:
+        album: str = song_title
+        for album_indicator in self.ALBUM_INDICATORS:
+            album = album.replace(album_indicator, "")
+
+        # return empty string any album indicator has been found in the song title
+        return album if album != song_title else ""
+
+    def parse(self, response: TextResponse) -> Generator[Dict[str, str], None, None]:
+        chart = response.xpath('//table[@id="tchart"]//tr/td[@class="artist"]')
+
+        for item in chart:
+
+            artist = item.xpath("b//text()").extract_first()
+            song_title = item.xpath("span//text()").extract_first().lower()
+
+            song = dict()
+            song["artist"] = artist
+            song["song_title"] = self._remove_text_between_parentheses(
+                self._remove_album_indicator(song_title)
+            )
+            song["album_title"] = self._remove_text_between_parentheses(
+                self._extract_album(song_title)
+            )
+            song["label"] = ""
+
+            yield song
